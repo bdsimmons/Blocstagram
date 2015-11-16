9bd65a6c55b6132ceb556d8c7a33fc173b30f1b0
Ben 'Bonesaw' Simmons <ben.simmons@trinityis.com>
2015-11-15 21:12:31 -0600

    Initial Commit

---
+++ Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Benjamin Simmons on 11/15/15.
+//  Copyright © 2015 Ben Simmons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Benjamin Simmons on 11/15/15.
+//  Copyright © 2015 Ben Simmons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
