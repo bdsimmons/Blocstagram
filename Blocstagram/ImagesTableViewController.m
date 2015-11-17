fbf5a76f1eb6bf3ae2a1c251ec0aa0a4913eea05
Ben 'Bonesaw' Simmons <ben.simmons@trinityis.com>
2015-11-17 09:39:32 -0600

    Complete checkpoint 16 building blocstagram

---
+++ Blocstagram/ImagesTableViewController.m
@@ -0,0 +1,82 @@
+//
+//  ImagesTableViewController.m
+//  Blocstagram
+//
+//  Created by Benjamin Simmons on 11/15/15.
+//  Copyright © 2015 Ben Simmons. All rights reserved.
+//
+
+#import "ImagesTableViewController.h"
+
+@interface ImagesTableViewController ()
+
+@end
+
+@implementation ImagesTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    for (int i = 1; i <= 10; i++) {
+        NSString *imageName = [NSString stringWithFormat:@"%d.jpg", i];
+        UIImage *image = [UIImage imageNamed:imageName];
+        if (image) {
+            [self.images addObject:image];
+        }
+    }
+    
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"imageCell"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (id)initWithStyle:(UITableViewStyle)style {
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+        self.images = [NSMutableArray array];
+    }
+    return self;
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.images.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"imageCell" forIndexPath:indexPath];
+    
+    // Configure the cell...
+    static NSInteger imageViewTag = 1234;
+    UIImageView *imageView = (UIImageView*)[cell.contentView viewWithTag:imageViewTag];
+    
+    if (!imageView) {
+        // This is a new cell, it doesn't have an image view yet
+        imageView = [[UIImageView alloc] init];
+        imageView.contentMode = UIViewContentModeScaleToFill;
+        
+        imageView.frame = cell.contentView.bounds;
+        imageView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+        
+        imageView.tag = imageViewTag;
+        [cell.contentView addSubview:imageView];
+    }
+    
+    UIImage *image = self.images[indexPath.row];
+    imageView.image = image;
+    
+    return cell;
+}
+
+- (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    UIImage *image = self.images[indexPath.row];
+    return (CGRectGetWidth(self.view.frame) / image.size.width) * image.size.height;
+}
+
+@end
