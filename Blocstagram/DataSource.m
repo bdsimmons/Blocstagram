2af3f83da99665faf3a22b06dec3cabba37e63d3
Ben 'Bonesaw' Simmons <ben.simmons@trinityis.com>
2015-11-28 09:29:07 -0600

    Complete assignment 20

--- Blocstagram/DataSource.m
+++ Blocstagram/DataSource.m
@@ -148,4 +148,10 @@
     [mutableArrayWithKVO removeObject:item];
 }
 
+- (void) moveMediaItemToTop:(Media *)item {
+    NSMutableArray *mutableArrayWithKVO = [self mutableArrayValueForKey:@"mediaItems"];
+    [mutableArrayWithKVO removeObject:item];
+    [mutableArrayWithKVO insertObject:item atIndex:0];
+}
+
 @end
--- Blocstagram/ImagesTableViewController.m
+++ Blocstagram/ImagesTableViewController.m
@@ -64,7 +64,7 @@
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source
         Media *item = [DataSource sharedInstance].mediaItems[indexPath.row];
-        [[DataSource sharedInstance] deleteMediaItem:item];
+        [[DataSource sharedInstance] moveMediaItemToTop:item];
     }
 }
 
